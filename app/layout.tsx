6c2360ce58e86c90e225d755fbb9cbdaecc7edaa
Eshwar Tangirala <eshwartangirala11@gmail.com>
2024-06-06 19:39:14 -0400

    changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Eshwar Tangirala',
   description: 'My website to showcase my skills and projects',
+  icons: {
+    icon: '/portfoliologo.jpeg', // Path to your favicon in the public directory
+  },
 }
 
 export default function RootLayout({
@@ -17,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/portfoliologo.jpeg" />
+      </head>
       <body className={inter.className}>
             {children}
             <ToasterContext />
